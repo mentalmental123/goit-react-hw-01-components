2a7e60418bf5b71e6ab2b41571ba063af12b5192
Alexey Luzanchuk <Luzanchuk123@gmail.com>
2023-06-20 18:15:37 +0300

    fix names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { App } from "./components/app/Apps";
+import { App } from "./components/App/Apps";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App></App>);
